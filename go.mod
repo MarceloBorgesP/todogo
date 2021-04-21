5989f3c596ee0a7972f7b6da06a09e185cac2344
Marcelo Borges <borgespereira.marcelo@gmail.com>
2021-04-21 13:08:24 -0300

    connect app to postgres and remove mock

--- go.mod
+++ go.mod
@@ -5,10 +5,8 @@ go 1.16
 require (
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator/v10 v10.4.1
-	github.com/google/uuid v1.2.0 // indirect
 	github.com/gorilla/mux v1.8.0
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/lithammer/shortuuid v3.0.0+incompatible // indirect
-	github.com/lithammer/shortuuid/v3 v3.0.6
+	github.com/lib/pq v1.10.0
 	gopkg.in/go-playground/validator.v9 v9.31.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,28 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/MarceloBorgesP/todogo/models"
 	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
-	"github.com/lithammer/shortuuid/v3"
+	_ "github.com/lib/pq"
 )
 
 type App struct {
 	Router *mux.Router
 	Todo   models.Todo
+	DB     *sql.DB
 }
 
 func main() {
 	a := App{}
-	a.Initialize()
+	a.Initialize(
+		// os.Getenv("POSTGRES_USER"),
+		// os.Getenv("POSTGRES_PASSWORD"),
+		// os.Getenv("POSTGRES_DB"))
+		"db", 5432, "postgres", "postgres", "postgres")
 	a.Run(":8080")
 }
 
-func (a *App) Initialize() {
+func (a *App) Initialize(host string, port uint16, user, password, dbname string) {
+	connectionString :=
+		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	var err error
+	a.DB, err = sql.Open("postgres", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	a.Router = mux.NewRouter()
 	a.initializeRoutes()
 }
@@ -41,17 +57,35 @@ func (a *App) initializeRoutes() {
 }
 
 func (app *App) getTasks(w http.ResponseWriter, r *http.Request) {
-	respondWithJSON(w, http.StatusOK, app.Todo.Tasks)
+	rows, err := app.DB.Query("SELECT * FROM tasks")
+
+	if err != nil {
+		respondWithJSON(w, http.StatusInternalServerError, err)
+	}
+
+	defer rows.Close()
+
+	tasks := []models.Task{}
+
+	for rows.Next() {
+		var task models.Task
+		if err := rows.Scan(&task.Id, &task.Name, &task.Description, &task.Status); err != nil {
+			respondWithJSON(w, http.StatusInternalServerError, err)
+		}
+		tasks = append(tasks, task)
+	}
+
+	respondWithJSON(w, http.StatusOK, &tasks)
 }
 
 func (app *App) createTask(w http.ResponseWriter, r *http.Request) {
 	decoder := json.NewDecoder(r.Body)
 	newTask := models.Task{}
+
 	err := decoder.Decode(&newTask)
 	if err != nil {
 		panic(err)
 	}
-	newTask.Id = shortuuid.New()
 
 	if errMessage := isValidInput(newTask); errMessage != nil {
 		respondWithError(w, errMessage)
@@ -59,21 +93,26 @@ func (app *App) createTask(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.Todo.Tasks = append(app.Todo.Tasks, newTask)
-	respondWithJSON(w, http.StatusCreated, newTask)
+	err = app.DB.QueryRow("INSERT INTO tasks(name, description, status) VALUES($1, $2, $3) RETURNING id", newTask.Name, newTask.Description, newTask.Status).Scan(&newTask.Id)
+
+	if err != nil {
+		respondWithError(w, err)
+	} else {
+		respondWithJSON(w, http.StatusCreated, newTask)
+	}
 }
 
 func (app *App) getTask(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	for _, task := range app.Todo.Tasks {
-		if task.Id == vars["id"] {
-			respondWithJSON(w, http.StatusOK, task)
+	var task models.Task
 
-			return
-		}
+	app.DB.QueryRow("SELECT * FROM tasks WHERE id=$1", vars["id"]).Scan(&task.Id, &task.Name, &task.Description, &task.Status)
+
+	if task.Id == "" {
+		respondWithJSON(w, http.StatusNotFound, nil)
+	} else {
+		respondWithJSON(w, http.StatusOK, task)
 	}
-
-	respondWithJSON(w, http.StatusNotFound, nil)
 }
 
 func (app *App) updateTask(w http.ResponseWriter, r *http.Request) {
@@ -91,47 +130,39 @@ func (app *App) updateTask(w http.ResponseWriter, r *http.Request) {
 	}
 
 	vars := mux.Vars(r)
-	for i, task := range app.Todo.Tasks {
-		if task.Id == vars["id"] {
-			app.Todo.Tasks[i].Name = updatedTask.Name
-			app.Todo.Tasks[i].Description = updatedTask.Description
-			app.Todo.Tasks[i].Status = updatedTask.Status
-			respondWithJSON(w, http.StatusOK, app.Todo.Tasks[i])
 
-			return
-		}
+	_, err = app.DB.Exec("UPDATE tasks SET name=$2, description=$3, status=$4 WHERE id=$1", vars["id"], updatedTask.Name, updatedTask.Description, updatedTask.Status)
+
+	if err == nil {
+		respondWithJSON(w, http.StatusOK, updatedTask)
+	} else {
+		respondWithJSON(w, http.StatusNotFound, nil)
 	}
-
-	respondWithJSON(w, http.StatusNotFound, nil)
 }
 
 func (app *App) deleteTask(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	for i, task := range app.Todo.Tasks {
-		if task.Id == vars["id"] {
-			app.Todo.Tasks[i] = app.Todo.Tasks[len(app.Todo.Tasks)-1]
-			app.Todo.Tasks = app.Todo.Tasks[:len(app.Todo.Tasks)-1]
-			respondWithJSON(w, http.StatusNoContent, nil)
 
-			return
-		}
+	_, err := app.DB.Exec("DELETE FROM tasks WHERE id=$1", vars["id"])
+
+	if err == nil {
+		respondWithJSON(w, http.StatusNoContent, nil)
+	} else {
+		respondWithJSON(w, http.StatusNotFound, nil)
 	}
 
-	respondWithJSON(w, http.StatusNotFound, nil)
 }
 
 func (app *App) completeTask(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
-	for i, task := range app.Todo.Tasks {
-		if task.Id == vars["id"] {
-			app.Todo.Tasks[i].Status = true
-			respondWithJSON(w, http.StatusNoContent, nil)
 
-			return
-		}
+	_, err := app.DB.Exec("UPDATE tasks SET status=true WHERE id=$1", vars["id"])
+
+	if err == nil {
+		respondWithJSON(w, http.StatusNoContent, nil)
+	} else {
+		respondWithJSON(w, http.StatusNotFound, nil)
 	}
-
-	respondWithJSON(w, http.StatusNotFound, nil)
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
@@ -140,7 +171,7 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	json.NewEncoder(w).Encode(payload)
 }
 
-func respondWithError(w http.ResponseWriter, errMessage map[string]string) {
+func respondWithError(w http.ResponseWriter, errMessage interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	respondWithJSON(w, http.StatusBadRequest, errMessage)
 }
