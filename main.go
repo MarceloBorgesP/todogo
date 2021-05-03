7f42bfb509257d8fc5076136b9518efbda0db5b1
Marcelo Borges <borgespereira.marcelo@gmail.com>
2021-05-03 07:33:08 -0300

    move sql queries to model

--- main.go
+++ main.go
@@ -4,9 +4,9 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 
 	"github.com/MarceloBorgesP/todogo/models"
@@ -17,25 +17,30 @@ import (
 
 type App struct {
 	Router *mux.Router
-	Todo   models.Todo
 	DB     *sql.DB
 }
 
 func main() {
 	a := App{}
 	a.Initialize(
-		os.Getenv("POSTGRES_HOSTNAME"),
-		os.Getenv("POSTGRES_PORT"),
-		os.Getenv("POSTGRES_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_DB"))
+		"localhost",
+		"5432",
+		"postgres",
+		"postgres",
+		"postgres")
 	a.Run(":8080")
 }
 
 func (a *App) Initialize(host, port, user, password, dbname string) {
 	portNumber, _ := strconv.Atoi(port)
 	connectionString :=
-		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, portNumber, user, password, dbname)
+		fmt.Sprintf(
+			"host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+			host,
+			portNumber,
+			user,
+			password,
+			dbname)
 
 	var err error
 	a.DB, err = sql.Open("postgres", connectionString)
@@ -61,111 +66,76 @@ func (a *App) initializeRoutes() {
 }
 
 func (app *App) getTasks(w http.ResponseWriter, r *http.Request) {
-	rows, err := app.DB.Query("SELECT * FROM tasks")
-
-	if err != nil {
+	if tasks, err := models.GetAll(app.DB); err != nil {
 		respondWithJSON(w, http.StatusInternalServerError, err)
-	}
-
-	defer rows.Close()
-
-	tasks := []models.Task{}
-
-	for rows.Next() {
-		var task models.Task
-		if err := rows.Scan(&task.Id, &task.Name, &task.Description, &task.Status); err != nil {
-			respondWithJSON(w, http.StatusInternalServerError, err)
-		}
-		tasks = append(tasks, task)
-	}
-
-	respondWithJSON(w, http.StatusOK, &tasks)
-}
-
-func (app *App) createTask(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	newTask := models.Task{}
-
-	err := decoder.Decode(&newTask)
-	if err != nil {
-		panic(err)
-	}
-
-	if errMessage := isValidInput(newTask); errMessage != nil {
-		respondWithError(w, errMessage)
-
-		return
-	}
-
-	err = app.DB.QueryRow("INSERT INTO tasks(name, description, status) VALUES($1, $2, $3) RETURNING id", newTask.Name, newTask.Description, newTask.Status).Scan(&newTask.Id)
-
-	if err != nil {
-		respondWithError(w, err)
 	} else {
-		respondWithJSON(w, http.StatusCreated, newTask)
+		respondWithJSON(w, http.StatusOK, tasks)
 	}
 }
 
 func (app *App) getTask(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
 	var task models.Task
+	vars := mux.Vars(r)
 
-	app.DB.QueryRow("SELECT * FROM tasks WHERE id=$1", vars["id"]).Scan(&task.Id, &task.Name, &task.Description, &task.Status)
-
-	if task.Id == "" {
+	if err := task.Get(app.DB, vars["id"]); err != nil {
 		respondWithJSON(w, http.StatusNotFound, nil)
 	} else {
 		respondWithJSON(w, http.StatusOK, task)
 	}
 }
 
-func (app *App) updateTask(w http.ResponseWriter, r *http.Request) {
-	decoder := json.NewDecoder(r.Body)
-	var updatedTask models.Task
-	err := decoder.Decode(&updatedTask)
+func (app *App) createTask(w http.ResponseWriter, r *http.Request) {
+	newTask, err := DecodeTask(r.Body)
 	if err != nil {
-		panic(err)
+		respondWithError(w, err)
+
+		return
 	}
 
-	if errMessage := isValidInput(updatedTask); errMessage != nil {
-		respondWithError(w, errMessage)
+	if err := newTask.Add(app.DB); err != nil {
+		respondWithError(w, err)
+	} else {
+		respondWithJSON(w, http.StatusCreated, newTask)
+	}
+}
+
+func (app *App) updateTask(w http.ResponseWriter, r *http.Request) {
+	updatedTask, err := DecodeTask(r.Body)
+	if err != nil {
+		respondWithError(w, err)
 
 		return
 	}
 
 	vars := mux.Vars(r)
 
-	_, err = app.DB.Exec("UPDATE tasks SET name=$2, description=$3, status=$4 WHERE id=$1", vars["id"], updatedTask.Name, updatedTask.Description, updatedTask.Status)
-
-	if err == nil {
-		respondWithJSON(w, http.StatusOK, updatedTask)
-	} else {
+	if errMessage := updatedTask.Update(app.DB, vars["id"]); errMessage != nil {
 		respondWithJSON(w, http.StatusNotFound, nil)
+	} else {
+		respondWithJSON(w, http.StatusOK, updatedTask)
 	}
 }
 
 func (app *App) deleteTask(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+	var task models.Task
 
-	_, err := app.DB.Exec("DELETE FROM tasks WHERE id=$1", vars["id"])
-
-	if err == nil {
-		respondWithJSON(w, http.StatusNoContent, nil)
-	} else {
+	if err := task.Delete(app.DB, vars["id"]); err != nil {
 		respondWithJSON(w, http.StatusNotFound, nil)
+	} else {
+		respondWithJSON(w, http.StatusNoContent, nil)
 	}
 
 }
 
 func (app *App) completeTask(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
+	var task models.Task
 
-	_, err := app.DB.Exec("UPDATE tasks SET status=true WHERE id=$1", vars["id"])
-
-	if err == nil {
-		respondWithJSON(w, http.StatusNoContent, nil)
-	} else {
+	if err := task.Complete(app.DB, vars["id"]); err != nil {
 		respondWithJSON(w, http.StatusNotFound, nil)
+	} else {
+		respondWithJSON(w, http.StatusNoContent, nil)
 	}
 }
 
@@ -178,6 +148,19 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 func respondWithError(w http.ResponseWriter, errMessage interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	respondWithJSON(w, http.StatusBadRequest, errMessage)
+}
+
+func DecodeTask(body io.ReadCloser) (models.Task, map[string]string) {
+	decoder := json.NewDecoder(body)
+	task := models.Task{}
+
+	err := decoder.Decode(&task)
+	if err != nil {
+		panic(err)
+	}
+
+	errMessage := isValidInput(task)
+	return task, errMessage
 }
 
 func isValidInput(task models.Task) map[string]string {
--- models/task.go
+++ models/task.go
@@ -1,6 +1,10 @@
 package models
 
-import "sync"
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
 
 type Task struct {
 	Id          string `json:"id"`
@@ -9,7 +13,84 @@ type Task struct {
 	Status      bool   `json:"status"`
 }
 
-type Todo struct {
-	mu    sync.Mutex
-	Tasks []Task
+func (task *Task) Add(db *sql.DB) error {
+	return db.QueryRow(
+		"INSERT INTO tasks(name, description, status) VALUES($1, $2, $3) RETURNING id, name, description, status",
+		task.Name,
+		task.Description,
+		task.Status).Scan(
+		&task.Id,
+		&task.Name,
+		&task.Description,
+		&task.Status)
+}
+
+func (task *Task) Get(db *sql.DB, id string) error {
+	return db.QueryRow(
+		"SELECT * FROM tasks WHERE id=$1",
+		id).Scan(
+		&task.Id,
+		&task.Name,
+		&task.Description,
+		&task.Status)
+}
+
+func (task *Task) Delete(db *sql.DB, id string) error {
+	result, err := db.Exec("DELETE FROM tasks WHERE id=$1", id)
+	if err != nil {
+		return err
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if rows == 0 {
+		return errors.New("not found")
+	}
+
+	return nil
+}
+
+func (task *Task) Update(db *sql.DB, id string) error {
+	return db.QueryRow(
+		"UPDATE tasks SET name=$2, description=$3, status=$4 WHERE id=$1 RETURNING id, name, description, status",
+		id,
+		task.Name,
+		task.Description,
+		task.Status).Scan(
+		&task.Id,
+		&task.Name,
+		&task.Description,
+		&task.Status)
+}
+
+func (task *Task) Complete(db *sql.DB, id string) error {
+	return db.QueryRow(
+		"UPDATE tasks SET status=true WHERE id=$1 RETURNING id, name, description, status",
+		id).Scan(
+		&task.Id,
+		&task.Name,
+		&task.Description,
+		&task.Status)
+}
+
+func GetAll(db *sql.DB) ([]Task, error) {
+	rows, err := db.Query("SELECT * FROM tasks")
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	tasks := []Task{}
+
+	for rows.Next() {
+		var task Task
+		if err := rows.Scan(&task.Id, &task.Name, &task.Description, &task.Status); err != nil {
+			return tasks, err
+		}
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
 }
